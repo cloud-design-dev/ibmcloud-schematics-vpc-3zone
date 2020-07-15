e98f7443163968b09bcd7b88b00ffe0df521eda8
greyhoundforty <ryantiffany@fastmail.com>
2020-07-15 13:13:20 -0500

    Adding instance deployments
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -19,30 +19,30 @@ module "subnets" {
 # }
 
 module "zone1_bastion_instance" {
-  source              = "./modules/04_instances"
-  vpc_name            = module.vpc.name
-  resource_group_name = var.resource_group_name
-  zone                = data.ibm_is_zones.regional_zones.zones[0]
-  subnet              = module.subnets.zone1_bastion_subnet
-  ssh_key             = var.ssh_key
+  source         = "./modules/04_instances"
+  vpc_name       = module.vpc.name
+  resource_group = var.resource_group
+  zone           = data.ibm_is_zones.regional_zones.zones[0]
+  subnet         = module.subnets.zone1_bastion_subnet
+  ssh_key        = var.ssh_key
 }
 
 module "zone2_bastion_instance" {
-  source              = "./modules/04_instances"
-  vpc_name            = module.vpc.name
-  resource_group_name = var.resource_group_name
-  zone                = data.ibm_is_zones.regional_zones.zones[1]
-  subnet              = module.subnets.zone1_bastion_subnet
-  ssh_key             = var.ssh_key
+  source         = "./modules/04_instances"
+  vpc_name       = module.vpc.name
+  resource_group = var.resource_group
+  zone           = data.ibm_is_zones.regional_zones.zones[1]
+  subnet         = module.subnets.zone1_bastion_subnet
+  ssh_key        = var.ssh_key
 }
 
 module "zone3_bastion_instance" {
-  source              = "./modules/04_instances"
-  vpc_name            = module.vpc.name
-  resource_group_name = var.resource_group_name
-  zone                = data.ibm_is_zones.regional_zones.zones[2]
-  subnet              = module.subnets.zone1_bastion_subnet
-  ssh_key             = var.ssh_key
+  source         = "./modules/04_instances"
+  vpc_name       = module.vpc.name
+  resource_group = var.resource_group
+  zone           = data.ibm_is_zones.regional_zones.zones[2]
+  subnet         = module.subnets.zone1_bastion_subnet
+  ssh_key        = var.ssh_key
 }
 
 # module "floating_ip" {
