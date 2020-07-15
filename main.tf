135264050e943ef581645ec1cf235568c5f3c105
greyhoundforty <ryantiffany@fastmail.com>
2020-07-15 08:02:14 -0500

    Initial schematics test build
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -12,21 +12,21 @@ module "subnets" {
   zones               = data.ibm_is_zones.regional_zones.zones
 }
 
-module "security" {
-  source              = "./modules/03_security"
-  vpc_name            = module.vpc.name
-  resource_group_name = var.resource_group_name
-}
+# module "security" {
+#   source              = "./modules/03_security"
+#   vpc_name            = module.vpc.name
+#   resource_group_name = var.resource_group_name
+# }
 
-module "bastion_instance" {
-  source              = "./modules/04_instances"
-  vpc_name            = module.vpc.name
-  resource_group_name = var.resource_group_name
-  zone                = data.ibm_is_zones.regional_zones.zones[0]
-}
+# module "bastion_instance" {
+#   source              = "./modules/04_instances"
+#   vpc_name            = module.vpc.name
+#   resource_group_name = var.resource_group_name
+#   zone                = data.ibm_is_zones.regional_zones.zones[0]
+# }
 
-module "floating_ip" {
-  source              = "./modules/05_floating_ip"
-  vpc_name            = module.vpc.name
-  resource_group_name = var.resource_group_name
-}
+# module "floating_ip" {
+#   source              = "./modules/05_floating_ip"
+#   vpc_name            = module.vpc.name
+#   resource_group_name = var.resource_group_name
+# }
