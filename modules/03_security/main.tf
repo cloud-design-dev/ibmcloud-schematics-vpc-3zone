fbd1dadb6254a8dacf82158045b9976b746ceb8f
greyhoundforty <ryantiffany@fastmail.com>
2020-07-15 06:45:01 -0500

    First layout commit
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- modules/03_security/main.tf
+++ modules/03_security/main.tf
@@ -1,29 +1,3 @@
-resource "ibm_is_public_gateway" "bastion_public_gateway" {
-  name           = "z1-${var.vpc_name}-gw"
-  resource_group = data.ibm_resource_group.default.id
-  vpc            = data.ibm_is_vpc.vpc.id
-  zone           = var.zone
-  tags           = [var.zone, var.vpc_name]
-}
-
-resource "ibm_is_subnet" "bastion_subnet" {
-  name            = "z1-${var.vpc_name}-bastion-subnet"
-  resource_group  = data.ibm_resource_group.default.id
-  vpc             = data.ibm_is_vpc.vpc.id
-  zone            = var.zone
-  ipv4_cidr_block = "10.240.0.0/28"
-  public_gateway  = ibm_is_public_gateway.bastion_public_gateway.id
-}
-
-resource "ibm_is_subnet" "private_subnet" {
-  name            = "z1-${var.vpc_name}-private-subnet"
-  resource_group  = data.ibm_resource_group.default.id
-  vpc             = data.ibm_is_vpc.vpc.id
-  zone            = var.zone
-  ipv4_cidr_block = "10.240.1.0/24"
-  public_gateway  = ibm_is_public_gateway.bastion_public_gateway.id
-}
-
 resource "ibm_is_security_group" "z1_bastion_sg" {
   name           = "z1-bastion-sg"
   resource_group = data.ibm_resource_group.default.id
--- modules/04_loadbalancer/data.tf
+++
@@ -1,7 +0,0 @@
-data "ibm_resource_group" "default" {
-  name = var.resource_group_name
-}
-
-data "ibm_is_vpc" "vpc" {
-  name = var.vpc_name
-}
